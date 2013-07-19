870b08844728f2e09809f365e0539b0e23237076
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2013-07-19 02:11:40 +0200

    make dispatcher work

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-INCLUDE=-I../upstream/oonf_api/src-api -I../upstream/oonf_api/build 
+INCLUDE=-I../upstream/oonf_api/src-api -I../upstream/oonf_api/build
 LIBDIR=../upstream/oonf_api/build
 
 CFLAGS=-Wall -std=c99 $(INCLUDE)
@@ -6,11 +6,13 @@ LDFLAGS=$(LIBDIR)/liboonf_rfc5444.so $(LIBDIR)/liboonf_common.so
 
 .PHONY: clean run
 
-main:	main.o node/node.o node/reader.o node/writer.o $(LDFLAGS)
+node:	node/node.o node/reader.o node/writer.o $(LDFLAGS)
+
+dispatcher:	dispatcher.o $(LDFLAGS)
 
 clean:
 	find -name '*.o' -type f -delete
-	rm -f olsr
+	rm dispatcher
 
-run:	main
-	LD_LIBRARY_PATH=$(LIBDIR) ./main
+run:	dispatcher
+	LD_LIBRARY_PATH=$(LIBDIR) ./dispatcher 9000
--- dispatcher.c
+++ dispatcher.c
@@ -5,6 +5,111 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "common/common_types.h"
+#include "common/netaddr.h"
+#include "rfc5444/rfc5444_print.h"
+
+/* for hexfump */
+static struct autobuf _hexbuf;
+
+struct node {
+	int id;
+	struct sockaddr_in addr;
+	socklen_t addr_len;
+
+	struct connection* connections;
+	struct node* next;
+};
+
+struct connection {
+	struct node* node;
+	struct connection* next;
+};
+
+static struct node* node_head = 0;
+
+static void connect_node(struct node* node_a, struct node* node_b, bool bidirectional) {
+	if (bidirectional)
+		connect_node(node_b, node_a, false);
+
+	printf("%d -> %d\n", node_a->id, node_b->id);
+
+	if (node_a->connections == 0) {
+		node_a->connections = malloc(sizeof (struct connection));
+		node_a->connections->node = node_b;
+		node_a->connections->next = 0;
+	} else {
+		struct connection* con = node_a->connections;
+		while (con->next)
+			con = con->next;
+		con->next = malloc(sizeof (struct connection));
+		con->next->node = node_b;
+		con->next->next = 0;
+	}
+}
+
+static struct node* add_node(int id) {
+	struct node* old_head = node_head;
+	node_head = malloc(sizeof(struct node));
+	node_head->next = old_head;
+	node_head->id = id;
+
+	return node_head;
+}
+
+static int add_node_data(struct sockaddr_in addr, socklen_t len) {
+	struct node *head = node_head;
+	do {
+		if (head->addr.sin_port == 0) {
+			head->addr = addr;
+			head->addr_len = len;
+
+			return head->id;
+		}
+	} while ((head = head->next));
+
+	return -1;
+}
+
+static int get_id(struct sockaddr_in addr) {
+	struct node *head = node_head;	
+
+	do {
+		if (head->addr.sin_port == addr.sin_port &&
+			head->addr.sin_addr.s_addr == addr.sin_addr.s_addr)
+			return head->id;
+	} while ((head = head->next));
+
+	return -1;
+}
+
+static void write_packet(int id, int socket, void *buffer, size_t length) {
+	printf("node: %d\n", id);
+
+	/* generate hexdump of packet */
+	abuf_hexdump(&_hexbuf, "\t", buffer, length);
+	rfc5444_print_direct(&_hexbuf, buffer, length);
+
+	/* print hexdump to console */
+	printf("%s", abuf_getptr(&_hexbuf));
+
+	struct node* head = node_head;
+	while (head) {
+		if (head->id == id && head->addr.sin_port) {
+			struct connection* con = head->connections;
+			while (con) {
+				if (con->node->addr.sin_port)
+					sendto(socket, buffer, length, 0, (struct sockaddr*) &con->node->addr, con->node->addr_len);
+				con = con->next;
+			}
+			return;
+		}
+		head = head->next;
+	}
+
+	printf("Error: can't write package - node not found\n");
+}
+
 static int setup_socket(int port) {
 	struct sockaddr_in si_me;
 	int s;
@@ -36,19 +141,34 @@ int main(int argc, char** argv) {
     	return -1;
 	}
 
+	/* initialize buffer for hexdump */
+	abuf_init(&_hexbuf);
+
+	int id = 0;
+	struct node* A = add_node(++id);
+	struct node* B = add_node(++id);
+	struct node* C = add_node(++id);
+
+	connect_node(A, B, true);
+	connect_node(A, C, true);
+
 	if ((socket = setup_socket(atoi(argv[1]))) < 0)
 		return -1;
 
 	struct sockaddr_in si_other;	
-	int n, slen = sizeof(si_other);
-	while (n = recvfrom(socket, buffer, sizeof buffer, 0, (struct sockaddr*) &si_other, &slen)) {
-	    buffer[n] = 0;
-	    printf("Data: %s\n", buffer);
-	  	printf("Received packet from %d\n", ntohs(si_other.sin_port));
-
-	  	sendto(socket, buffer, n, 0, (struct sockaddr*) &si_other, slen);
+	int n;
+	socklen_t slen = sizeof(si_other);
+	while ((n = recvfrom(socket, buffer, sizeof buffer, 0, (struct sockaddr*) &si_other, &slen))) {
+		int id = get_id(si_other);
+		if (id < 0)
+			id = add_node_data(si_other, slen);
+		if (id < 0)
+			printf("ignoring unknown node");
+		else
+			write_packet(get_id(si_other), socket, buffer, n);	
 	}
 	
 	close(socket);
+	abuf_free(&_hexbuf);
 	return 0;
 }
