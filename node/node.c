57cae85a405ff5519586cd1afe177bfec0b644e7
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2013-07-19 03:56:12 +0200

    make nodes send and receive Hello World over UDP

--- node/node.c
+++ node/node.c
@@ -1,8 +1,13 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
 #include "writer.h"
 #include "reader.h"
@@ -31,6 +36,39 @@ void init_socket(in_addr_t addr, int port) {
    servaddr.sin_port = htons(port);
 }
 
+void sigio_handler(int sig) {
+   char buffer[1500];
+   int length;
+
+   if ((length = recvfrom(sockfd, &buffer, sizeof buffer, 0, 0, 0)) == -1)
+       return;
+   buffer[length] = 0;
+   printf("Received: %s", buffer);
+}
+
+int enable_asynch(int sock) {
+	int flags;
+	struct sigaction sa;
+
+	flags = fcntl(sock, F_GETFL);
+	fcntl(sock, F_SETFL, flags | O_ASYNC); 
+
+	sa.sa_flags = 0;
+	sa.sa_handler = sigio_handler;
+	sigemptyset(&sa.sa_mask);
+
+	if (sigaction(SIGIO, &sa, NULL))
+		return -1;
+
+	if (fcntl(sock, F_SETOWN, getpid()) < 0)
+		return -1;
+
+	if (fcntl(sock, F_SETSIG, SIGIO) < 0)
+		return -1;
+	
+	return 0;
+}
+
 int main(int argc, char** argv) {
 	if (argc != 3) {
 		printf("usage:  %s <IP address> <port>\n", argv[0]);
@@ -38,11 +76,20 @@ int main(int argc, char** argv) {
 	}
 
 	init_socket(inet_addr(argv[1]), atoi(argv[2]));
+	enable_asynch(sockfd);
 
 	reader_init();
 	writer_init(write_packet);
 
-	writer_tick();
+	char buffer[] = "Hello World!\n";
+	while (1) {
+		int remaining_sleep = 5;
+		while (remaining_sleep = sleep(remaining_sleep));
+
+		printf("Sending %s", buffer);
+		sendto(sockfd, buffer, sizeof buffer, 0, (struct sockaddr*) &servaddr, sizeof(servaddr));
+//		writer_tick();
+	}
 
 	reader_cleanup();
 	writer_cleanup();
