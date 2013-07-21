6260d6e1e0150681826b4a842c16098a6404bdd8
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2013-07-21 17:17:08 +0200

    fix add_tail and list_remove, add find_list

--- node/list.c
+++ node/list.c
@@ -6,24 +6,47 @@ struct list_elem {
 	struct list_elem* next;
 };
 
-void* add_tail(struct list_elem* head, size_t size) {
-	while (head) {
-		head = head->next;
+void* add_tail(struct list_elem** head, size_t size) {
+	struct list_elem* _head = *head;
+
+	if (!_head)
+		return *head = malloc(size);
+
+	while (_head->next) {
+		_head = _head->next;
 	}
 
-	return head = malloc(size);
+	return _head->next = malloc(size);
 }
 
-void list_remove(struct list_elem* head, struct list_elem* node) {
-	struct list_elem* prev = 0;
-	while (head && head != node) {
-		prev = head;
+void* find_list(struct list_elem* head, void* needle, int offset) {
+	while (head) {
+		void** buff = (void*) head + offset;
+
+		if (*buff ==  needle)
+			return head;
 		head = head->next;
 	}
 
-	if (head != node)
+	return 0;
+}
+
+void list_remove(struct list_elem** head, struct list_elem* node) {
+	struct list_elem* _head = *head;
+	struct list_elem* prev = 0;
+
+	while (_head && _head != node) {
+		prev = _head;
+		_head = _head->next;
+	}
+
+	if (_head != node)
 		return; // not found
 
-	prev->next = node->next;
+	if (!prev)	// remove head
+		*head = _head->next;
+	else
+		prev->next = node->next;
+
 	free(node);
 }
--- node/list.h
+++ node/list.h
@@ -4,6 +4,7 @@
 
 struct list_elem;
 
-void* add_tail(struct list_elem* head, size_t size);
-void list_remove(struct list_elem* head, struct list_elem* node);
+void* add_tail(struct list_elem** head, size_t size);
+void* find_list(struct list_elem* head, void* needle, int offset);
+void list_remove(struct list_elem** head, struct list_elem* node);
 #endif /* LIST_H_ */
