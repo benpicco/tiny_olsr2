0dad5dedb89cd55f31cf9edc023dbbf26aa4239d
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2013-10-05 14:26:08 +0200

    prepare a more sane way of refcounting

--- node/util.c
+++ node/util.c
@@ -2,7 +2,21 @@
 #include "util.h"
 
 struct netaddr* netaddr_dup(struct netaddr* addr) {
-	struct netaddr* addr_new = calloc(1, sizeof(struct netaddr));
+	struct netaddr_rc* addr_new = calloc(1, sizeof(struct netaddr_rc));
+	addr_new->_refs = 1;
 	return memcpy(addr_new, addr, sizeof(struct netaddr));
 }
 
+struct netaddr* netaddr_use(struct netaddr* addr) {
+	((struct netaddr_rc*) addr)->_refs++;
+	return addr;
+}
+
+struct netaddr* netaddr_free(struct netaddr* addr) {
+	struct netaddr_rc* addr_rc = (struct netaddr_rc*) addr;
+	if (--addr_rc->_refs == 0) {
+		free(addr_rc);
+		return 0;
+	}
+	return addr;
+}
--- node/util.h
+++ node/util.h
@@ -3,6 +3,13 @@
 
 #include "common/netaddr.h"
 
+struct netaddr_rc {
+	struct netaddr super;
+	uint8_t _refs;
+};
+
 struct netaddr* netaddr_dup(struct netaddr* addr);
+struct netaddr* netaddr_use(struct netaddr* addr);
+struct netaddr* netaddr_free(struct netaddr* addr);
 
 #endif
