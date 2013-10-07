2beac0824072a4b5f5b64dff155b7de7021ff25e
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2013-10-07 14:34:17 +0200

    fix paranthesis

--- node/list.h
+++ node/list.h
@@ -8,13 +8,13 @@ struct simple_list_elem;
 #define simple_list_add_head(head)	_simple_list_add_head((struct simple_list_elem**) head, sizeof **head)
 #define simple_list_add_tail(head)	_simple_list_add_tail((struct simple_list_elem**) head, sizeof **head)
 #define simple_list_add_before(head, value) *head == 0 ? simple_list_add_head(head) : \
-	_simple_list_add_before((struct simple_list_elem**) head, sizeof **head, value, (void*) &(*head)->value - (void*) *head)
+	_simple_list_add_before((struct simple_list_elem**) head, sizeof **head, value, (void*) &(*(head))->value - (void*) *head)
 #define simple_list_find(head, value)	head == 0 ? NULL : \
-	_simple_list_find((struct simple_list_elem*) head, value, (void*) &head->value - (void*) head, 0)
+	_simple_list_find((struct simple_list_elem*) head, value, (void*) &((head)->value) - (void*) head, 0)
 #define simple_list_find_memcmp(head, value)	head == 0 ? NULL : \
-	_simple_list_find((struct simple_list_elem*) head, value, (void*) &head->value - (void*) head, sizeof(*value))
+	_simple_list_find((struct simple_list_elem*) head, value, (void*) &((head)->value) - (void*) head, sizeof(*value))
 #define simple_list_find_cmp(head, value, comperator)	head == 0 ? NULL : \
-	_simple_list_find_cmp((struct simple_list_elem*) head, value, (void*) &head->value - (void*) head, comperator)
+	_simple_list_find_cmp((struct simple_list_elem*) head, value, (void*) &((head)->value) - (void*) head, comperator)
 #define simple_list_remove(head, node)	_simple_list_remove((struct simple_list_elem**) head, (struct simple_list_elem*) node)
 
 #define simple_list_for_each(head, node)	for(node = head; node; node = node->next)
