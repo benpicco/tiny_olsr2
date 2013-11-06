f889639170ae3687d5c4e51baaaa8884fbb047d3
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2013-11-06 18:51:23 +0100

    toggle LED on send & receive

--- node/olsr2/olsr_init.c
+++ node/olsr2/olsr_init.c
@@ -15,6 +15,7 @@
 #include <destiny.h>
 #include <random.h>
 #include <config.h>
+#include <board.h>
 
 #include "rfc5444/rfc5444_writer.h"
 
@@ -57,6 +58,7 @@ static void write_packet(struct rfc5444_writer *wr __attribute__ ((unused)),
 	struct rfc5444_writer_target *iface __attribute__((unused)),
 	void *buffer, size_t length) {
 
+	LED_GREEN_TOGGLE;
 	int bytes_send = destiny_socket_sendto(sock, buffer, length, 0, &sa_bcast, sizeof sa_bcast);
 
 	DEBUG("write_packet(%d bytes), %d bytes sent", length, bytes_send);
@@ -83,6 +85,7 @@ static void olsr_receiver_thread(void) {
 
 	while (1) {
 		recsize = destiny_socket_recvfrom(sock, &buffer, sizeof buffer, 0, &sa, &fromlen);
+		LED_RED_TOGGLE;
 
 		memcpy(&_src._addr, &sa.sin6_addr, sizeof _src._addr);
 		DEBUG("received %d bytes from %s", recsize, netaddr_to_str_s(&nbuf[0], &_src));
